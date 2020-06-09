2dfbe1f9e97bc813a6839be410286ba5f8baf855
code-loading <eahmed711@gmail.com>
2020-06-09 21:55:25 +0400

    first upload to master

---
+++ src/Room.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+//import './App.css';
+
+function Room() {
+    //const [isLit, setLit] = React.useState(false);
+    const [isLit, setLit] = React.useState(true);
+    //const brightness = isLit ? "lit" : "dark";
+    const [isTemp, setTemp] = React.useState(22);
+    
+    return (
+      <div className={`room ${isLit ? "lit" : "dark"}`}>
+        The room is {isLit ? "lit" : "dark"}
+        <br />
+        The temperature is {isTemp}
+        <br />
+        <button onClick={() => setLit(!isLit)}>Toggle light</button>
+        <br />
+        <button onClick={() => setLit(true)}>Turn on lights</button>
+        <br />
+        <button onClick={() => setLit(false)}>Turn off lights</button>
+        <br />
+        <button onClick={() => setTemp(isTemp + 1)}>+</button>
+        <button onClick={() => setTemp(isTemp - 1)}>-</button>
+      </div>
+    );
+  }
+export default Room;
